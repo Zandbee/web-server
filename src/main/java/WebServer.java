b8ea509a3baabb2f7372f34dc55d076897d4e454
Veronika Strokova <veronika.strokova@dataart.com>
2016-06-16 19:07:40 +0300

    Thread is broken

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -11,36 +11,58 @@ import java.util.logging.Logger;
  */
 public class WebServer {
     private static final Logger logger = Logger.getLogger(WebServer.class.getName());
+
     private static final String PROPERTIES_FILE = "config.properties";
-    private static final int DEFAULT_PORT = 4444;
+    private static final String PROPERTIES_KEY_PORT = "port";
+    private static final String PROPERTIES_KEY_HOST = "host";
+    private static final String PROPERTIES_KEY_SESSION_INTERVAL = "session_interval_seconds";
+
+    private static final String DEFAULT_PORT = "4444";
+    private static final String DEFAULT_HOST = "D:/web-server";
+    private static final String DEFAULT_SESSION_INTERVAL = "60";
 
     public static void main(String[] args) {
         logger.info("Starting a server");
-        listen(getPort());
+
+        // use port and host from config file
+        Properties properties = getPropertiesList();
+        listen(getPort(properties), getHost(properties), getSessionInterval(properties));
     }
 
-    private static void listen(int portNumber) {
+    private static void listen(int portNumber, String homeDir, int sessionInterval) {
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             while (true) {
-                new WebServerThread(serverSocket.accept()).run();
+                System.out.println("START NEW THREAD");
+                new WebServerThread(serverSocket.accept(), homeDir, sessionInterval).run();
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Cannot listen on port " + portNumber, e);
         }
     }
 
-    private static int getPort() {
+    private static int getPort(Properties properties) {
+        return Integer.parseInt(properties.getProperty(PROPERTIES_KEY_PORT, DEFAULT_PORT));
+    }
+
+    private static String getHost(Properties properties) {
+        return properties.getProperty(PROPERTIES_KEY_HOST, DEFAULT_HOST);
+    }
+
+    private static int getSessionInterval(Properties properties) {
+        return Integer.parseInt(properties.getProperty(PROPERTIES_KEY_SESSION_INTERVAL, DEFAULT_SESSION_INTERVAL));
+    }
+
+    private static Properties getPropertiesList() {
         Properties properties = new Properties();
-        int port = DEFAULT_PORT;
-        //WebServer.class.getResourceAsStream()
+
         try (FileInputStream in = new FileInputStream(PROPERTIES_FILE)) {
             properties.load(in);
-            port = Integer.parseInt(properties.getProperty("port", Integer.toString(DEFAULT_PORT)));
         } catch (FileNotFoundException e) {
             logger.log(Level.SEVERE, "Properties file not found: " + PROPERTIES_FILE, e);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error processing " + PROPERTIES_FILE, e);
         }
-        return port;
+
+        return properties;
     }
 }
--- src/main/java/WebServerThread.java
+++ src/main/java/WebServerThread.java
@@ -3,6 +3,9 @@ import java.net.Socket;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,44 +16,73 @@ public class WebServerThread extends Thread {
     private static final Logger logger = Logger.getLogger(WebServerThread.class.getName());
     private static final String UTF_8 = "UTF-8";
     private static final String REQUEST_GET = "GET";
-    private static final String URI_SCHEME_FILE = "file:";
-    private static final String URI_HOST = "///D:/web-server";
-    private static final String INDEX_HTML = "/index.html";
-    private Socket socket;
 
-    public WebServerThread(Socket socket) {
+    private static final String HEADER_DATE = "Date: ";
+    private static final String HEADER_SERVER = "Server: ";
+
+    private static final String URI_SCHEME_FILE = "file:///";
+    private static final String HTML_INDEX = "/index.html";
+    private static final String HTML_FORBIDDEN = "/index.html";
+    private static final String SERVER_NAME = "noname.server.ru";
+
+    private Socket socket;
+    private String host; // files are stored here
+    private int sessionInterval;
+    private long sessionStartTime;
+    private long currentTime;
+
+    public WebServerThread(Socket socket, String host, int sessionInterval) {
         //super("WebServerThread"); TODO
+        System.out.println("NEW THREAD");
         this.socket = socket;
+        this.host = host;
+        this.sessionInterval = sessionInterval;
+        this.sessionStartTime = System.currentTimeMillis();
     }
 
     @Override
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
              OutputStream out = socket.getOutputStream()) {
-            String requestLine = in.readLine();
+            String requestLine;
 
-            if (requestLine != null && requestLine.startsWith(REQUEST_GET)) {
-                int pathStart = requestLine.indexOf(" ") + 1;
-                int pathFinish = requestLine.indexOf(" ", pathStart);
-                String path = requestLine.substring(pathStart, pathFinish);
-                System.out.println("Requested path: " + path);
+            while ((requestLine = in.readLine()) != null) {
+                // check if a session has not ended yet
+                /*long curTime = System.currentTimeMillis();
+                int sessionTime = (int) (curTime - sessionStartTime);
+                System.out.println("Start time: " + sessionStartTime);
+                System.out.println("Current time: " + curTime);
+                System.out.println("Session time: " + sessionTime);
+                if (sessionTime > sessionInterval) {
+                    // session timeout
+                    respondForbidden(out);
+                    return;
+                }*/
 
-                respond(out, path);
+                if (requestLine.startsWith(REQUEST_GET)) {
+                    int pathStart = requestLine.indexOf(" ") + 1;
+                    int pathFinish = requestLine.indexOf(" ", pathStart);
+                    String path = requestLine.substring(pathStart, pathFinish);
+                    System.out.println("Requested path: " + path);
+
+                    respond(out, path);
+                }
             }
+            System.out.println("Null in request line");
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error reading from or writing to the socket", e);
         }
     }
 
-    // TODO: send socket/stream, not bw
     private void respond(OutputStream out, String path) {
         switch (path) {
             case "/":
-                //return index.html
-                respondOk(out, URI.create(URI_SCHEME_FILE + URI_HOST + INDEX_HTML));
+                // return index.html
+                respondOk(out, URI.create(URI_SCHEME_FILE + host + HTML_INDEX));
                 break;
             default:
-                URI fileUri = URI.create(URI_SCHEME_FILE + URI_HOST + path);
+                // return requested file
+                URI fileUri = URI.create(URI_SCHEME_FILE + host + path);
                 System.out.println("File URI: " + fileUri);
                 if (Files.exists(Paths.get(fileUri))) {
                     respondOk(out, fileUri);
@@ -59,14 +91,17 @@ public class WebServerThread extends Thread {
                 }
                 break;
         }
-
     }
 
     private void respondFileNotFound(OutputStream out) {
         try (BufferedWriter bw = new BufferedWriter(new PrintWriter(out))) {
             System.out.println("RESPONDING - File not found");
             bw.write("HTTP/1.1 404 File not found");
-            bw.newLine();
+            bw.newLine(); // empty line after status line
+
+            writeResponseHeaders(bw);
+
+            bw.flush();
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error writing to the socket", e);
         }
@@ -77,7 +112,8 @@ public class WebServerThread extends Thread {
             System.out.println("RESPONDING");
             bw.write("HTTP/1.1 200 OK");
             bw.newLine(); // empty line after status line
-            bw.newLine(); // empty line after headers
+
+            writeResponseHeaders(bw);
 
             BufferedReader br = new BufferedReader(new FileReader(new File(fileUri.getPath())));
             String fileLine;
@@ -91,5 +127,40 @@ public class WebServerThread extends Thread {
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error writing to the socket", e);
         }
+    }
+
+    private void respondForbidden(OutputStream out) {
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter(out))) {
+            System.out.println("RESPONDING - Forbidden/Session expired");
+            bw.write("HTTP/1.1 403 Forbidden");
+            bw.newLine(); // empty line after status line
+
+            writeResponseHeaders(bw);
+
+            BufferedReader br = new BufferedReader(new FileReader(new File(URI.create(URI_SCHEME_FILE + host + HTML_FORBIDDEN))));
+            String fileLine;
+            while ((fileLine = br.readLine()) != null) {
+                bw.write(fileLine);
+                bw.newLine();
+            }
+
+            bw.flush();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error writing to the socket", e);
+        }
+    }
+
+    // TODO: can forget insert this in some respond... method
+    private void writeResponseHeaders(BufferedWriter bw) throws IOException { // TODO: static?
+        // any server information
+        bw.write(HEADER_SERVER + SERVER_NAME);
+        bw.newLine();
+
+        // current server time
+        SimpleDateFormat httpDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
+        bw.write(HEADER_DATE + httpDateFormat.format(new Date()));
+        bw.newLine();
+
+        bw.newLine(); // empty line after headers
     }
 }
